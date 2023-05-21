300279f8280bce72c2545940874ceecee9930cf9
kavabean <gdegruccio@gmail.com>
2023-05-21 00:48:00 -0400

    Included username sorting

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ from ignored import ignoredAPI
 import os
 
 
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message, username, is_private=False):
 
     try:
-        response = responses.get_response(user_message)
+        response = responses.get_response(user_message, username)
         await message.author.send(response) if is_private else await message.channel.send(response)
 
     except Exception as e:
@@ -47,9 +47,9 @@ def run_discord_bot():
 
         if user_message[0] == '*':
             user_message = user_message[1:]
-            await send_message(message, user_message, is_private=False)
+            await send_message(message, user_message, username, is_private=False)
 
         if user_message:
-            await send_message(message, user_message, is_private=False)
+            await send_message(message, user_message, username, is_private=False)
 
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,11 +1,22 @@
 import random
 
 
-def get_response(message: str) -> str:
+def get_response(message: str, username) -> str:
 
+    user = username.lower()
     p_message = message.lower()
-
+    rando = random.randint(1, 30)
+    print(rando)
     if 'hanni' in p_message:
-        return 'mid lmao'
+        return 'mid'
+
+    if rando == 1:
+        return 'hanni is mid'
+
+    if rando == 2:
+        return f'"{message}" is something a fool would say.'
+
+    if rando == 21 and 'joomillion' in username:
+        return 'midmillion'
 
     return
