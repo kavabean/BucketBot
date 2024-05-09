ee7266e42982bd2bda2a7ca50581a05daa802053
kavabean <gdegruccio@gmail.com>
2024-05-09 15:30:13 -0400

    asus local

--- responses.py
+++ responses.py
@@ -5,18 +5,15 @@ def get_response(message: str, username) -> str:
 
     user = username.lower()
     p_message = message.lower()
-    rando = random.randint(1, 30)
+    rando = random.randint(1, 15)
     print(rando)
-    if 'hanni' in p_message:
-        return 'mid'
-
-    if rando == 1:
-        return 'hanni is mid'
 
     if rando == 2:
         return f'"{message}" is something a fool would say.'
 
-    if rando == 21 and 'joomillion' in username:
-        return 'midmillion'
+    if rando == 15 and 'goob' in username:
+        return 'You look like a merper not gonna lie'
 
+    if 'jiya' in username:
+        return 'You look very pretty today, as always!'
     return
